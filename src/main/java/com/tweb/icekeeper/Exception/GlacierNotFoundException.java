90c7a85d06f5ec5df91570993c747959da256aaf
Andrei <76631255+John-Andrey@users.noreply.github.com>
2025-01-29 21:25:57 +0100

    Delete src directory

--- src/main/java/com/tweb/icekeeper/Exception/GlacierNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.tweb.icekeeper.Exception;
-
-public class GlacierNotFoundException  extends  RuntimeException {
-    public GlacierNotFoundException(Long id) {
-        super("Glacier non trovato con ID: " + id);
-    }
-}
